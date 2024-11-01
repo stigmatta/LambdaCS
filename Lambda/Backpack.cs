04770371fbe425a9c0b97f5057a220f1f01d2090
Andrey Odintsov <morrigun0@gmail.com>
2024-11-02 00:59:41 +0200

    Add project files.

---
+++ Lambda/Backpack.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackpackClass
+{
+    public delegate void AddHandler(BackpackObj obj);
+
+    public class Arrays
+    {
+        public static string[] Fabrics = { "Cotton", "Silk", "Wool", "Linen", "Polyester" };
+    }
+
+    public class BackpackObj
+    {
+        private int volume;
+        private string name;
+
+        public int Volume
+        {
+            get => volume;
+            set
+            {
+                if (value > 0)
+                {
+                    volume = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name can't be null or empty");
+                }
+                name = value;
+            }
+        }
+
+        public BackpackObj(string name, int volume)
+        {
+            Name = name;
+            Volume = volume;
+        }
+    }
+
+    public class Backpack
+    {
+        private string color, manufactor, fabrics;
+        private int weight, volume;
+        private int occupiedVolume;
+        private List<BackpackObj> objects;
+
+        public event AddHandler ObjectAdded;
+
+        public string Color
+        {
+            get => color;
+            set
+            {
+                if (Enum.TryParse<ConsoleColor>(value, true, out _))
+                {
+                    color = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Color isn't valid");
+                }
+            }
+        }
+
+        public string Manufactor
+        {
+            get => manufactor;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Manufactor can't be null or empty");
+                }
+                manufactor = value;
+            }
+        }
+
+        public string Fabrics
+        {
+            get => fabrics;
+            set
+            {
+                if (Array.Exists(Arrays.Fabrics, fabric => fabric.Equals(value, StringComparison.OrdinalIgnoreCase)))
+                {
+                    fabrics = value;
+                }
+                else
+                {
+                    throw new ArgumentException("There is no such fabric");
+                }
+            }
+        }
+
+        public int Weight
+        {
+            get => weight;
+            set
+            {
+                if (value > 0)
+                {
+                    weight = value;
+                }
+            }
+        }
+
+        public int Volume
+        {
+            get => volume;
+            set
+            {
+                if (value > 0)
+                {
+                    volume = value;
+                }
+            }
+        }
+
+        public Backpack(string color, string manufactor, string fabrics, int weight, int volume)
+        {
+            Color = color;
+            Manufactor = manufactor;
+            Fabrics = fabrics;
+            Weight = weight;
+            Volume = volume;
+            occupiedVolume = 0;
+            objects = new List<BackpackObj>();
+
+            ObjectAdded += delegate (BackpackObj obj)
+            {
+                Console.WriteLine($"Object '{obj.Name}' added to the backpack.");
+            };
+        }
+
+        public void AddObject(BackpackObj obj)
+        {
+            if (obj == null)
+            {
+                Console.WriteLine("Object cannot be null.");
+                return;
+            }
+
+            if (occupiedVolume + obj.Volume > Volume)
+            {
+                throw new InvalidOperationException("Not enough volume in the backpack to add this object.");
+            }
+
+            objects.Add(obj);
+            occupiedVolume += obj.Volume;
+            ObjectAdded?.Invoke(obj);
+        }
+    }
+}
---
+++ Lambda/Program.cs
@@ -0,0 +1,87 @@
+﻿
+using BackpackClass;
+using System;
+
+namespace Lambda
+{
+    public delegate void ConsoleRGB(string str);
+    public delegate bool ArrayLambda(int i);
+    public delegate bool WordFinder(string str,string text);
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            ConsoleRGB consoleRGB = delegate (string str)
+            {
+                if (Enum.TryParse<ConsoleColor>(str, true, out ConsoleColor color))
+                {
+                    Console.BackgroundColor = color;
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine($"'{str}' is not a valid console color.");
+                }
+            };
+
+            consoleRGB("Cyan");
+            #endregion
+
+            #region Task2
+            BackpackObj obj = new BackpackObj("Pen", 1);
+            Backpack backpack = new Backpack("Cyan", "Adidas", "Cotton", 50, 100);
+
+            backpack.AddObject(obj);
+
+            #endregion
+
+            #region Task3
+            int[] arr = {-3,-3,-5,-7, 7, 14, 3, 5, 6, 7 };
+            ArrayLambda isDivisibleBySeven = (num) => num % 7 == 0;
+            int sevenCounter = 0;
+            foreach (int i in arr)
+            {
+                if (isDivisibleBySeven(i))
+                    sevenCounter++;
+            }
+            Console.WriteLine(sevenCounter);
+            #endregion
+
+            #region Task4
+            ArrayLambda IsPositive = (num) => num > 0;
+            int posCounter = 0;
+
+            foreach(int i in arr)
+            {
+                if(IsPositive(i)) posCounter++;
+            }
+            Console.WriteLine(posCounter);
+            #endregion
+
+            #region Task5
+            ArrayLambda NegativeDistinct = (num) => num < 0;
+            int negCounter = 0;
+
+            foreach (int i in arr)
+            {
+                if (IsPositive(i)) negCounter++;
+            }
+            Console.WriteLine(negCounter);
+
+            #endregion
+
+            #region Task6
+            string text = "Hello world apple orange";
+            string str1 = "orange";
+            string str2 = "porridge";
+            WordFinder wordFinder = (strstr, str) => str.Contains(strstr);
+            Console.WriteLine(wordFinder(str1,text));
+            Console.WriteLine(wordFinder(str2, text));
+
+
+            #endregion
+        }
+    }
+}
